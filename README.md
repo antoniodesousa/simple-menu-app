e92ad6e0bb47d7adbe09b96349ce187f6f88e801
Antonio de Sousa <antoniojsba@gmail.com>
2021-03-06 11:55:32 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,49 @@
+# Simple Menu App
+
+A simple page with a menu and a sticky footer.
+
+## Technical details
+
+This application makes use of Webpack, Babel and Eslint.
+The code base is writing in Typescript and all the styles in SASS.
+The application should work correctly in the latest versions of Chrome, Firefox and Edge.
+
+## Set up the application
+
+Make sure you have NodeJS installed in your machine.
+Create a folder with this project and execute this command lines in the terminal:
+
+```bash
+$ cd your-project-folder
+$ npm install
+$ npm run build (or npm run prod)
+$ npm start
+```
+
+At this point, the application should be available in your browser in this url:
+
+```
+http://localhost:3000/
+```
+
+## Testing
+
+Type this command line to run the test cases:
+
+```bash
+$ npm run test
+```
+
+Type this command line to create a coverage report:
+
+```bash
+$ npm run coverage
+```
+
+## Linting
+
+Type this command line:
+
+```bash
+$ npm run eslint
+```
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const buildMode = process.env.NODE_ENV || 'development';
+const isProduction = buildMode === 'production';
+
+module.exports = {
+   mode: buildMode,
+   entry: './src/index.ts',
+   devtool: isProduction ? 'inline-source-map' : 'source-map',
+   module: {
+	  rules: [
+		 {
+			test: /\.(ts|tsx)?$/u,
+			use: 'ts-loader',
+			exclude: [path.resolve(__dirname, 'node_modules')]
+		 },
+		 {
+			test: /\.js$/u,
+			exclude: [path.resolve(__dirname, 'node_modules')],
+			loader: 'babel-loader'
+		 },
+		 {
+			test: /\.(css|scss)$/u,
+			use: [
+			   MiniCssExtractPlugin.loader,
+			   'css-loader',
+			   'sass-loader'
+			]
+		 },
+		 {
+			test: /\.(ico|png|jpg)$/u,
+			loader: 'file-loader'
+		 },
+		 {
+			test: /\.html$/u,
+			loader: 'html-loader'
+		 }
+	  ]
+   },
+   resolve: {
+	  extensions: ['.ts', '.tsx', '.js', '.scss', '.css']
+   },
+   plugins: [
+	  new CleanWebpackPlugin({
+		 cleanStaleWebpackAssets: false
+	  }),
+	  new ESLintPlugin({
+		 failOnError: isProduction,
+		 emitError: true,
+		 emitWarning: true
+	  }),
+	  new MiniCssExtractPlugin({
+		 filename: 'styles.css'
+	  }),
+	  new HtmlWebpackPlugin({
+		 filename: 'index.html',
+		 template: './src/template.html',
+		 favicon: './src/assets/favicon.ico',
+		 minify: isProduction,
+		 inject: true,
+		 hash: true
+	  })
+   ],
+   output: {
+	  path: path.resolve(__dirname, 'dist/'),
+	  publicPath: '',
+	  filename: 'bundle.js'
+   },
+   devServer: {
+	  contentBase: path.join(__dirname, 'dist/'),
+	  publicPath: '/',
+	  port: 3000,
+	  writeToDisk: true
+   },
+   optimization: {
+	  minimizer: [
+		 '...',
+		 new CssMinimizerPlugin()
+	  ]
+   }
+};
