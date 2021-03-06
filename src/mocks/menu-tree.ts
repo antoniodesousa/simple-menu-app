9c78d64a95692261c57b1280b0796c5db901da8f
Antonio de Sousa <antoniojsba@gmail.com>
2021-03-07 00:22:10 +0100

    Create menu tree mock data

---
+++ src/mocks/menu-tree.ts
@@ -0,0 +1,7 @@
+import { IMenuProps } from '../library/menu-item';
+
+export const menuTree: IMenuProps[] = [
+	{ title: 'Menu 1', subItems: ['Sub-menu 1.1', 'Sub-menu 1.2', 'Sub-menu 1.3'] },
+	{ title: 'Menu 2', subItems: ['Sub-menu 2.1', 'Sub-menu 2.2', 'Sub-menu 2.3'] },
+	{ title: 'Menu 3', subItems: ['Sub-menu 3.1', 'Sub-menu 3.2', 'Sub-menu 3.3'] }
+];
