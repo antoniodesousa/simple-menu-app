f55f0f8f1a41011a5700780503f52e2b7d5ccb89
Antonio de Sousa <antoniojsba@gmail.com>
2021-03-07 11:23:46 +0100

    Render footer in the page

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import './styles/index.css';
 import { navigationBar } from './components/navigation-bar';
 import { contentWrapper } from './components/content-wrapper';
+import { footer } from './components/footer';
 
 document.addEventListener('DOMContentLoaded', (): void => {
 	const root: HTMLElement = document.getElementById('root');
+
 	root.appendChild(navigationBar());
 	root.appendChild(contentWrapper());
+	root.appendChild(footer());
 });
