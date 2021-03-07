a0ca5d3914e2e8d2abba79001502c944b43eca01
Antonio de Sousa <antoniojsba@gmail.com>
2021-03-07 11:09:26 +0100

    Create content wrapper component

---
+++ src/components/content-wrapper.ts
@@ -0,0 +1,26 @@
+import { loremIpsum } from '../mocks/lorem-ipsum';
+
+const contentWrapper = (): HTMLElement => {
+	const pageContent: HTMLElement = document.createElement('div');
+	pageContent.className = 'content-wrapper';
+
+	const paragraph1 = generateContent();
+	const paragraph2 = generateContent();
+
+	pageContent.innerHTML = `<p>${paragraph1}</p><p>${paragraph2}</p>`;
+
+	return pageContent;
+};
+
+function generateContent(): string {
+	const text = [];
+	let x = 30;
+
+	while (--x) {
+		text.push(loremIpsum[Math.floor(Math.random() * loremIpsum.length)]);
+	}
+
+	return text.join(' ');
+}
+
+export { contentWrapper };
