9dedfbf0b78eed5a6240f7f20f7b3a33d5297a6d
Antonio de Sousa <antoniojsba@gmail.com>
2021-03-07 11:08:32 +0100

    Create lorem ipsum mock data

---
+++ src/mocks/lorem-ipsum.ts
@@ -0,0 +1,15 @@
+export const loremIpsum: string[] = [
+	'Duis viverra tincidunt lectus.',
+	'Aenean sollicitudin ullamcorper quam, sed egestas nunc congue quis.',
+	'Morbi malesuada, purus nec porttitor mattis, odio orci ornare nunc, at commodo enim diam non leo.',
+	'Morbi maximus non velit euismod gravida.',
+	'Donec commodo massa tincidunt justo commodo, scelerisque vestibulum ligula blandit.',
+	'Ut eu ullamcorper libero, eget gravida libero.',
+	'Aenean dignissim scelerisque luctus.',
+	'Quisque non blandit justo.',
+	'Mauris tortor magna, tristique eu fermentum nec, condimentum ac diam.',
+	'Nullam sed ex sollicitudin, tempor urna sit amet, vestibulum libero.',
+	'Mauris viverra erat et purus facilisis, in aliquam nisl fringilla.',
+	'Aliquam sollicitudin enim et turpis euismod luctus.',
+	'Aenean a suscipit felis.'
+];
