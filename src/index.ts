77fae2c01f7d47ebc849094850f09004894320a4
Antonio de Sousa <antoniojsba@gmail.com>
2021-03-07 11:09:59 +0100

    Render content wrapper in the page

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import './styles/index.css';
 import { navigationBar } from './components/navigation-bar';
+import { contentWrapper } from './components/content-wrapper';
 
 document.addEventListener('DOMContentLoaded', (): void => {
 	const root: HTMLElement = document.getElementById('root');
 	root.appendChild(navigationBar());
+	root.appendChild(contentWrapper());
 });
